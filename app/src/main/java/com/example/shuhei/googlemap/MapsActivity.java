5abb57a1ebe95ffa95f515d0020e5a06b6133697
shuhei <shuhei@shuhei-no-MacBook-Pro.local>
2015-06-23 03:27:18 +0900

    third commit

--- app/src/main/java/com/example/shuhei/googlemap/MapsActivity.java
+++ app/src/main/java/com/example/shuhei/googlemap/MapsActivity.java
@@ -2,7 +2,6 @@ package com.example.shuhei.googlemap;
 
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -11,6 +10,7 @@ import android.view.View;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,43 +20,60 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+//import android.location.LocationListener;
+
 public class MapsActivity extends FragmentActivity
         implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
+    // GoogleApiClient.ConnectionCallbacks をConnectionCallbacksだけにすると
+    // べつのクラスが呼ばれてGoogleApiClient.Builderでエラーになる
 
     public GoogleMap mMap; // Might be null if Google Play services APK is not available.
     public GoogleMap mMap2;
 
-    private FusedLocationProviderApi fusedLocationProviderApi = LocationServices.FusedLocationApi;
+    private boolean mResolvingError = false;
     private LatLng mKansai = new LatLng(34.435912, 135.243496);
     private LatLng mItami = new LatLng(34.785500, 135.438004);
     private LatLng hankyurokkou = new LatLng(34.71985,135.234388);
 
     private GoogleApiClient mLocationClient = null;
+    private FusedLocationProviderApi fusedLocationProviderApi = LocationServices.FusedLocationApi;
+    private LocationRequest locationRequest;
+    private Location location;
+/*
     private static final LocationRequest REQUEST = LocationRequest.create()
             .setInterval(5000)
             .setFastestInterval(16)
             .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
 
+        // LocationRequest を生成して精度、インターバルを設定
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(16);
+
         mLocationClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+
+
         if(mLocationClient != null){
             Log.d("testdesu", "d");
 
             mLocationClient.connect();
             Log.d("testdesu", "e");
         }
+
 
     }
 
@@ -69,7 +86,7 @@ public class MapsActivity extends FragmentActivity
         //thread
       //  (new Thread(new Task())).start();
 
-
+        Log.d("onCreate()¥n", "testdesu");
     }
 
 
@@ -99,6 +116,7 @@ public class MapsActivity extends FragmentActivity
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
+                mMap.setMyLocationEnabled(true);
             }
         }
 
@@ -122,7 +140,7 @@ public class MapsActivity extends FragmentActivity
     private void setUpMap() {
        // mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
 
-        LatLng latLng = new LatLng(35.684699, 139.753897);
+        LatLng latLng = new LatLng(34.71985,135.234388);
         float zoom = 13; // 2.0～21.0
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
 
@@ -132,7 +150,7 @@ public class MapsActivity extends FragmentActivity
        // mMap2.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
 
         // 初期座標、拡大率設定
-        LatLng latLng = new LatLng(35.684699, 139.753897);
+        LatLng latLng = new LatLng(34.71985,135.234388);
         float zoom = 13; // 2.0～21.0
         mMap2.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
 
@@ -141,16 +159,132 @@ public class MapsActivity extends FragmentActivity
     }
 
     public void plus(View v) {
-        LatLng latLng = new LatLng(35.684699, 139.753897);
+        LatLng latLng = new LatLng(34.71985,135.234388);
         mMap2.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, mMap.getCameraPosition().zoom));
 
     }
 
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+
+        if (!mResolvingError) {
+            // Connect the client.
+            mLocationClient.connect();
+
+            Log.d("onStart()¥n connect()¥n", "testdesu");
+        }
+        else{
+            Log.d("onStart()¥n mResolvingError ¥n", "testdesu");
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        Log.d("onStop()¥n", "testdesu");
+
+
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d("onConnected()¥n", "testdesu");
+
+        Location currentLocation = fusedLocationProviderApi.getLastLocation(mLocationClient);
+        if (currentLocation != null && currentLocation.getTime() > 20000) {
+            location = currentLocation;
+
+            Log.d("----------\n", "testdesu");
+            Log.d("Latitude="+ String.valueOf(location.getLatitude())+"\n", "testdesu");
+            Log.d("Longitude="+ String.valueOf(location.getLongitude())+"\n", "testdesu");
+            Log.d("Accuracy="+ String.valueOf(location.getAccuracy())+"\n", "testdesu");
+            Log.d("Altitude=" + String.valueOf(location.getAltitude()) + "\n", "testdesu");
+            Log.d("Time="+ String.valueOf(location.getTime())+"\n", "testdesu");
+            Log.d("Speed=" + String.valueOf(location.getSpeed())+"\n", "testdesu");
+            Log.d("Bearing=" + String.valueOf(location.getBearing())+"\n", "testdesu");
+
+
+        } else {
+            Log.d("koko", "testdesu");
+            fusedLocationProviderApi.requestLocationUpdates(mLocationClient, locationRequest, (com.google.android.gms.location.LocationListener) this);
+            Log.d("kokodesu", "testdesu");
+
+
+
+            // Schedule a Thread to unregister location listeners
+            Executors.newScheduledThreadPool(1).schedule(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d("----------\n", "testdesu");
+                    fusedLocationProviderApi.removeLocationUpdates(mLocationClient, (com.google.android.gms.location.LocationListener) MapsActivity.this);
+
+                    location = fusedLocationProviderApi.getLastLocation(mLocationClient);
+
+                    Log.d("----------\n", "testdesu");
+                    Log.d("Latitude="+ String.valueOf(location.getLatitude())+"\n", "testdesu");
+                    Log.d("Longitude="+ String.valueOf(location.getLongitude())+"\n", "testdesu");
+                    Log.d("Accuracy="+ String.valueOf(location.getAccuracy())+"\n", "testdesu");
+                    Log.d("Altitude=" + String.valueOf(location.getAltitude()) + "\n", "testdesu");
+                    Log.d("Time="+ String.valueOf(location.getTime())+"\n", "testdesu");
+                    Log.d("Speed=" + String.valueOf(location.getSpeed()) + "\n", "testdesu");
+                    Log.d("Bearing=" + String.valueOf(location.getBearing())+"\n", "testdesu");
+                }
+            }, 10000, TimeUnit.MILLISECONDS);
+
+
+            Log.d("onConnected(), requestLocationUpdates \n", "testdesu");
+
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        Log.d("----------\n", "testdesu");
+                    Log.d("Latitude="+ String.valueOf(location.getLatitude())+"\n", "testdesu");
+                    Log.d("Longitude="+ String.valueOf(location.getLongitude())+"\n", "testdesu");
+                    Log.d("Accuracy="+ String.valueOf(location.getAccuracy())+"\n", "testdesu");
+                    Log.d("Altitude=" + String.valueOf(location.getAltitude()) + "\n", "testdesu");
+                    Log.d("Time="+ String.valueOf(location.getTime())+"\n", "testdesu");
+                    Log.d("Speed=" + String.valueOf(location.getSpeed()) + "\n", "testdesu");
+                    Log.d("Bearing=" + String.valueOf(location.getBearing())+"\n", "testdesu");
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("onConnectionSuspended()\n", "testdesu");
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d("onConnectionFailed())\n", "testdesu");
+
+
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            Log.d("Already attempting to resolve an error","testdesu");
+
+            return;
+        } else if (connectionResult.hasResolution()) {
+
+        } else {
+            mResolvingError = true;
+        }
+    }
+*/
 
     @Override
     public void onConnected(Bundle bundle) {
         Log.d("testdesu", "coonect");
-        fusedLocationProviderApi.requestLocationUpdates(mLocationClient, REQUEST, (com.google.android.gms.location.LocationListener) this);
+        fusedLocationProviderApi.requestLocationUpdates(mLocationClient, locationRequest, (com.google.android.gms.location.LocationListener) this);
         Log.d("testdesu", "connected");
     }
 
@@ -182,24 +316,10 @@ public class MapsActivity extends FragmentActivity
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
+
 }
 
 
