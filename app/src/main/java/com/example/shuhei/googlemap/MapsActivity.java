480d27772cf5462a0b38d5be7698c5406a372c66
shuhei <shuhei@shuhei-no-MacBook-Pro.local>
2015-06-26 18:41:52 +0900

    0626 gamenseni

--- app/src/main/java/com/example/shuhei/googlemap/MapsActivity.java
+++ app/src/main/java/com/example/shuhei/googlemap/MapsActivity.java
@@ -118,7 +118,7 @@ public class MapsActivity extends FragmentActivity
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.map);
-        fragment.getView().setAlpha((float)seekBar.getProgress()/(float)10);
+        fragment.getView().setAlpha((float) seekBar.getProgress() / (float) 10);
 
     }
 
@@ -224,127 +224,33 @@ public class MapsActivity extends FragmentActivity
         mMap2.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, mMap.getCameraPosition().zoom));
         */
     }
+    public void showMap1(View v) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.map);
+        fragment.getView().setAlpha(1.0f);
+
+        Fragment fragment2 = getSupportFragmentManager().findFragmentById(R.id.map2);
+
+        fragment.getView().setTop(0);
+        fragment.getView().setBottom(1088);
+        fragment2.getView().setTop(0);
+        fragment2.getView().setBottom(0);
+    }
+    public void showMap2(View v) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.map);
+        //fragment.getView().setAlpha(1.0f);
+
+        Fragment fragment2 = getSupportFragmentManager().findFragmentById(R.id.map2);
+
+        fragment.getView().setTop(0);
+        fragment.getView().setBottom(0);
+        fragment2.getView().setTop(0);
+        fragment2.getView().setBottom(1088);
+    }
     public void setZoom(View v) {
         mMap2.animateCamera(CameraUpdateFactory.zoomTo(mMap.getCameraPosition().zoom));
     }
 
-    /*
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Connect the client.
-
-        if (!mResolvingError) {
-            // Connect the client.
-            mLocationClient.connect();
-
-            Log.d("onStart()¥n connect()¥n", "testdesu");
-        }
-        else{
-            Log.d("onStart()¥n mResolvingError ¥n", "testdesu");
-
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        // Disconnecting the client invalidates it.
-        mLocationClient.disconnect();
-        Log.d("onStop()¥n", "testdesu");
-
-
-        super.onStop();
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Log.d("onConnected()¥n", "testdesu");
-
-        Location currentLocation = fusedLocationProviderApi.getLastLocation(mLocationClient);
-        if (currentLocation != null && currentLocation.getTime() > 20000) {
-            location = currentLocation;
-
-            Log.d("----------\n", "testdesu");
-            Log.d("Latitude="+ String.valueOf(location.getLatitude())+"\n", "testdesu");
-            Log.d("Longitude="+ String.valueOf(location.getLongitude())+"\n", "testdesu");
-            Log.d("Accuracy="+ String.valueOf(location.getAccuracy())+"\n", "testdesu");
-            Log.d("Altitude=" + String.valueOf(location.getAltitude()) + "\n", "testdesu");
-            Log.d("Time="+ String.valueOf(location.getTime())+"\n", "testdesu");
-            Log.d("Speed=" + String.valueOf(location.getSpeed())+"\n", "testdesu");
-            Log.d("Bearing=" + String.valueOf(location.getBearing())+"\n", "testdesu");
-
-
-        } else {
-            Log.d("koko", "testdesu");
-            fusedLocationProviderApi.requestLocationUpdates(mLocationClient, locationRequest, (com.google.android.gms.location.LocationListener) this);
-            Log.d("kokodesu", "testdesu");
-
-
-
-            // Schedule a Thread to unregister location listeners
-            Executors.newScheduledThreadPool(1).schedule(new Runnable() {
-                @Override
-                public void run() {
-                    Log.d("----------\n", "testdesu");
-                    fusedLocationProviderApi.removeLocationUpdates(mLocationClient, (com.google.android.gms.location.LocationListener) MapsActivity.this);
-
-                    location = fusedLocationProviderApi.getLastLocation(mLocationClient);
-
-                    Log.d("----------\n", "testdesu");
-                    Log.d("Latitude="+ String.valueOf(location.getLatitude())+"\n", "testdesu");
-                    Log.d("Longitude="+ String.valueOf(location.getLongitude())+"\n", "testdesu");
-                    Log.d("Accuracy="+ String.valueOf(location.getAccuracy())+"\n", "testdesu");
-                    Log.d("Altitude=" + String.valueOf(location.getAltitude()) + "\n", "testdesu");
-                    Log.d("Time="+ String.valueOf(location.getTime())+"\n", "testdesu");
-                    Log.d("Speed=" + String.valueOf(location.getSpeed()) + "\n", "testdesu");
-                    Log.d("Bearing=" + String.valueOf(location.getBearing())+"\n", "testdesu");
-                }
-            }, 10000, TimeUnit.MILLISECONDS);
-
-
-            Log.d("onConnected(), requestLocationUpdates \n", "testdesu");
-
-        }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-        Log.d("----------\n", "testdesu");
-                    Log.d("Latitude="+ String.valueOf(location.getLatitude())+"\n", "testdesu");
-                    Log.d("Longitude="+ String.valueOf(location.getLongitude())+"\n", "testdesu");
-                    Log.d("Accuracy="+ String.valueOf(location.getAccuracy())+"\n", "testdesu");
-                    Log.d("Altitude=" + String.valueOf(location.getAltitude()) + "\n", "testdesu");
-                    Log.d("Time="+ String.valueOf(location.getTime())+"\n", "testdesu");
-                    Log.d("Speed=" + String.valueOf(location.getSpeed()) + "\n", "testdesu");
-                    Log.d("Bearing=" + String.valueOf(location.getBearing()) + "\n", "testdesu");
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d("onConnectionSuspended()\n", "testdesu");
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.d("onConnectionFailed())\n", "testdesu");
-
-
-        if (mResolvingError) {
-            // Already attempting to resolve an error.
-            Log.d("Already attempting to resolve an error","testdesu");
-
-            return;
-        } else if (connectionResult.hasResolution()) {
-
-        } else {
-            mResolvingError = true;
-        }
-    }
-*/
-
+   
     @Override
     public void onConnected(Bundle bundle) {
         Log.d("testdesu", "coonect");
